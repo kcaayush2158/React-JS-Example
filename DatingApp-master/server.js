af89ec735daf6267cbbec8dfb7a37c73907f7646
System Administrator <aayushkc2158@gmail.com>
2021-06-10 16:56:54 +0545

    bug fixes

--- DatingApp-master/server.js
+++ DatingApp-master/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/DatingApp'));
+app.use(express.static('./dist/dating'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/dating/'}),
 );
 
 // Start the app by listening on the default Heroku port
